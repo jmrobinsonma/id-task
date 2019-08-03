02702e1c5cffb754d6661f4e494c4db0f4813141
Jason Robinson <jmrobinsonma@gmail.com>
2019-08-03 13:34:18 -0400

    fix csv formatting

--- id_task.py
+++ id_task.py
@@ -4,7 +4,8 @@ import csv
 csv_filename = 'id_task.csv'
 csv_file = open(csv_filename, 'w')
 csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['ID Number'])
+
+id_list = []
 
 with open('data (1).json', 'r') as f:
 	data_list = json.load(f)
@@ -12,8 +13,9 @@ with open('data (1).json', 'r') as f:
 for nested_dict in data_list:
 	for key in nested_dict:
 		if "id" in key:
-			csv_writer.writerow([nested_dict['id']])
+			id_list.append(nested_dict['id'])
 
+csv_writer.writerow(id_list)
 csv_file.close()
 
 print(f'The data has been saved to {csv_filename}')
