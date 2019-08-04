1554dfd85eec4b3703e323f72d4fe4a50c0a1bfd
Jason Robinson <jmrobinsonma@gmail.com>
2019-08-04 19:24:14 -0400

    added try-except

--- id_task.py
+++ id_task.py
@@ -1,19 +1,32 @@
 import json
 import csv
 
+json_filename = 'data.json'
 csv_filename = 'id_task.csv'
+id_list = []
+exceptions = 0
+
 csv_file = open(csv_filename, 'w')
 csv_writer = csv.writer(csv_file)
 
-id_list = []
+try:
+	with open(json_filename, 'r') as f:
+		data_list = json.load(f)
 
-with open('data (1).json', 'r') as f:
-	data_list = json.load(f)
+except Exception as err:
+	exceptions += 1
+	print(f"\nSorry, there was an error while loading the input file: \n{err}\n")	
 
-for nested_dict in data_list:
-	id_list.append(nested_dict['id'])
+else:
+	for nested_dict in data_list:
+		id_list.append(nested_dict['id'])
 
 csv_writer.writerow(id_list)
-csv_file.close()
 
-print(f'The data has been saved to {csv_filename}')
+if exceptions == 1:
+	pass
+
+else:
+	print(f"\nThe data was saved to {csv_filename}")
+
+csv_file.close()
