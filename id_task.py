b711c84354d1466d2261581e38d02a9239c0b8e5
Jason Robinson <jmrobinsonma@gmail.com>
2019-08-07 16:57:05 -0400

    open input file now before create output file

--- id_task.py
+++ id_task.py
@@ -1,32 +1,28 @@
 import json
 import csv
+import sys
 
 json_filename = 'data.json'
 csv_filename = 'id_task.csv'
 id_list = []
-exceptions = 0
-
-csv_file = open(csv_filename, 'w')
-csv_writer = csv.writer(csv_file)
 
 try:
 	with open(json_filename, 'r') as f:
 		data_list = json.load(f)
 
 except Exception as err:
-	exceptions += 1
-	print(f"\nSorry, there was an error while loading the input file: \n{err}\n")	
+	print(f"\nSorry, there was an error while loading the input file: \n{err}\n-Please check to make sure there are no spelling errors\n-Make sure that the file is in the current directory")	
+	sys.exit()
 
-else:
-	for nested_dict in data_list:
-		id_list.append(nested_dict['id'])
+csv_file = open(csv_filename, 'w')
+csv_writer = csv.writer(csv_file)
+
+
+for nested_dict in data_list:
+	id_list.append(nested_dict['id'])
 
 csv_writer.writerow(id_list)
 
-if exceptions == 1:
-	pass
-
-else:
-	print(f"\nThe data was saved to {csv_filename}")
+print(f"\nThe data was saved to {csv_filename}")
 
 csv_file.close()
