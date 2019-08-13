67d03c5a03ed6557431d6a90eac437b5b8222cce
Jason Robinson <jmrobinsonma@gmail.com>
2019-08-13 19:19:36 -0400

    refactored the script to be a function

--- id_task.py
+++ id_task.py
@@ -2,24 +2,27 @@ import json
 import csv
 import sys
 
+def id_parser(input_json, output_csv):
+	try:
+		with open(input_json, 'r') as f:
+			data_list = json.load(f)
 
-try:
-	with open('data.json', 'r') as f:
-		data_list = json.load(f)
+	except Exception as err:
+		print(f"\nSorry, there was an error while loading the input file:\
+			 \n{err}\
+			 \n-Please check to make sure there are no spelling errors\
+			 \n-Make sure that the file is in the current directory")	
+		sys.exit()
 
-except Exception as err:
-	print(f"\nSorry, there was an error while loading the input file: \n{err}\n-Please check to make sure there are no spelling errors\n-Make sure that the file is in the current directory")	
-	sys.exit()
+	csv_file = open(output_csv, 'w')
+	csv_writer = csv.writer(csv_file)
 
-csv_filename = 'id_task.csv'
+	id_list = [key['id'] for key in data_list]
 
-csv_file = open(csv_filename, 'w')
-csv_writer = csv.writer(csv_file)
+	csv_writer.writerow(id_list)
 
-id_list = [key['id'] for key in data_list]
+	print(f"\nThe data was saved to {output_csv}")
 
-csv_writer.writerow(id_list)
+	csv_file.close()
 
-print(f"\nThe data was saved to {csv_filename}")
-
-csv_file.close()
+id_parser('data.json', 'id_task.csv')
