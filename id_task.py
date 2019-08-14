ea3af17c2e7b668bdd0b82792299d43b6d9f7f39
Jason Robinson <jmrobinsonma@gmail.com>
2019-08-13 20:12:11 -0400

    added argparse

--- id_task.py
+++ id_task.py
@@ -1,10 +1,18 @@
 import json
 import csv
 import sys
+import argparse
 
-def id_parser(input_json, output_csv):
+
+parser = argparse.ArgumentParser(description='This function will copy all id numbers\
+		 from a .json input file which will be stored in a .csv file.')
+parser.add_argument('in_file', type=str, help='enter an existing .json file')
+parser.add_argument('out_file', type=str,  help='enter a name to create or overwrite a file (file_name.csv)')
+args = parser.parse_args()
+
+def id_parser(in_file, out_file):
 	try:
-		with open(input_json, 'r') as f:
+		with open(in_file, 'r') as f:
 			data_list = json.load(f)
 
 	except Exception as err:
@@ -14,15 +22,15 @@ def id_parser(input_json, output_csv):
 			 \n-Make sure that the file is in the current directory")	
 		sys.exit()
 
-	csv_file = open(output_csv, 'w')
+	csv_file = open(out_file, 'w')
 	csv_writer = csv.writer(csv_file)
 
 	id_list = [key['id'] for key in data_list]
 
 	csv_writer.writerow(id_list)
-
-	print(f"\nThe data was saved to {output_csv}")
-
 	csv_file.close()
 
-id_parser('data.json', 'id_task.csv')
+	return print(f"\nThe data was saved to {out_file}")
+	 
+
+id_parser(args.in_file, args.out_file)
