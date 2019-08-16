f33b5b72020dc34b9f04dc3f116e1bb55e3b0865
Jason Robinson <jmrobinsonma@gmail.com>
2019-08-16 08:50:58 -0400

    changed variable names, closed .json file, edited exception text

--- id_task.py
+++ id_task.py
@@ -10,27 +10,30 @@ parser.add_argument('in_file', type=str, help='enter an existing .json file')
 parser.add_argument('out_file', type=str,  help='enter a name to create or overwrite a file (file_name.csv)')
 args = parser.parse_args()
 
-def id_parser(in_file, out_file):
+def id_extractor(in_file, out_file):
 	try:
-		with open(in_file, 'r') as f:
-			data_list = json.load(f)
+		with open(in_file, 'r') as json_file:
+			data_list = json.load(json_file)
 
 	except Exception as err:
-		print(f"\nSorry, there was an error while loading the input file:\
-			 \n{err}\
-			 \n-Please check to make sure there are no spelling errors\
-			 \n-Make sure that the file is in the current directory")	
+		print(f"Sorry, there was an error while loading the input file:\
+			\n{err}\
+			\n-Specify a path to a valid .json file\
+			\n-If the file is in the current directory use <filename>.json\
+			-Please check to make sure there are no spelling errors")
 		sys.exit()
 
 	csv_file = open(out_file, 'w')
 	csv_writer = csv.writer(csv_file)
 
-	id_generator = tuple((key['id'] for key in data_list))
+	extractor = tuple((key['id'] for key in data_list))
 
-	csv_writer.writerow(id_generator)
+	csv_writer.writerow(extractor)
+	
 	csv_file.close()
-
+	json_file.close()
+	
 	return print(f"\nThe data was saved to {out_file}")
 	 
 
-id_parser(args.in_file, args.out_file)
+id_extractor(args.in_file, args.out_file)
