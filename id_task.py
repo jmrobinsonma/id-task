6f413a0c5a9141330eb45ca38ace3df81f772c28
Jason Robinson <jmrobinsonma@gmail.com>
2019-09-05 18:31:53 -0400

    refactored the with open blocks

--- id_task.py
+++ id_task.py
@@ -19,7 +19,9 @@ def id_extractor(in_file, out_file):
 	try:
 		with open(in_file, 'r') as json_file:
 			data_list = json.load(json_file)
-
+			with open(out_file, 'w') as csv_file:
+				csv_writer = csv.writer(csv_file)
+				csv_writer.writerow((key['id'] for key in data_list))
 	except Exception as err:
 		print(f"Sorry, there was an error while loading the input file:\
 			\n{err}\
@@ -27,16 +29,8 @@ def id_extractor(in_file, out_file):
 			\n-If the file is in the current directory use <filename>.json\
 			-Please check to make sure there are no spelling errors")
 		sys.exit()
-
-	with open(out_file, 'w') as csv_file:
-		csv_writer = csv.writer(csv_file)
-
-	extractor = tuple((key['id'] for key in data_list))
-
-	csv_writer.writerow(extractor)
 	
 	if args.verbose:
 		return print(f"\nThe data was saved to {out_file}")
-	 
 
 id_extractor(args.in_file, args.out_file)
